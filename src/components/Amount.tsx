ace256962b1c84a41a2c2371dca2572260253707
Christopher Henn <chris@chrishenn.net>
2022-08-23 19:27:03 -0400

    Slightly less dumb rounding

--- src/components/Amount.tsx
+++ src/components/Amount.tsx
@@ -5,7 +5,27 @@ export function Amount({
   quantity: number | null;
   units: string | null;
 }) {
-  return (
-    <span>{quantity ? `${quantity}${units ? ` ${units}` : ""} ` : ""}</span>
-  );
+  if (!quantity) {
+    return null;
+  }
+
+  const isDecimal = Math.round(quantity) !== quantity;
+
+  if (isDecimal && units) {
+    return (
+      <span>
+        {quantity.toFixed(2).replace(/0+$/, "")} {units}
+      </span>
+    );
+  }
+
+  if (units) {
+    return (
+      <span>
+        {quantity} {units}
+      </span>
+    );
+  }
+
+  return <span>{quantity}</span>;
 }
