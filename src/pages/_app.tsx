b9c0672bc877ef6e27e9400d9a8d4196487e6cf4
Christopher Henn <chris@chrishenn.net>
2022-09-18 10:17:42 -0400

    Add Safari-specific meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import "./_app.css";
@@ -7,9 +8,15 @@ const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="theme-color" content="#fff" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 };
 
